7544e25f7ae313b5a4e76c0d90c03ad1e6f2f14b
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-09-26 12:50:44 +0200

    Implemented simple server

--- modules/server/include/lcxx/server.hpp
+++ modules/server/include/lcxx/server.hpp
@@ -3,6 +3,7 @@
 
 #include <functional>
 #include <string>
+#include <unordered_map>
 
 #include <boost/beast.hpp>
 
@@ -10,24 +11,46 @@
 
 namespace lcxx {
 
-    using request = std::string;
+    using request    = boost::beast::http::request< boost::beast::http::dynamic_body >;
+    using response   = boost::beast::http::response< boost::beast::http::dynamic_body >;
+    using get_cb     = std::function< void( request const & ) >;
+    using put_cb     = std::function< void( request const & ) >;
+    using get_cb_map = std::unordered_map< std::string, get_cb >;
+    using put_cb_map = std::unordered_map< std::string, get_cb >;
 
     class server {
+
     public:
         enum class run_option {
             async,
             sync,
         };
 
-        server( std::string const & ip, uint16_t port );
+        server( std::string const & ip, uint16_t port, std::optional< boost::asio::io_context > & ioc );
 
-        void on_get_endpoint( std::string const & endpoint, std::function< void() > callback );
-        void on_put_endpoint( std::string const & endpoint, std::function< void() > callback );
+        void on_get_endpoint( std::string const & endpoint, get_cb callback );
+        void on_put_endpoint( std::string const & endpoint, put_cb callback );
 
-        license read_license( request const & req, crypto::rsa_key_t private_key );
-        license write_license( request & req, crypto::rsa_key_t private_key );
+        auto read_license( request const & req, crypto::rsa_key_t private_key ) -> license;
+        auto write_license( response & resp, crypto::rsa_key_t private_key ) -> license;
 
         void run( run_option ro );
+        void stop();
+
+    private:
+        void                   handle_loop();
+        net::awaitable< void > handle_loop_coro();
+
+        std::shared_ptr< boost::asio::io_context > ioc_;
+        boost::asio::ip::tcp::acceptor             acceptor_;
+        boost::asio::ip::tcp::socket               socket_;
+
+        run_option  ro_;
+        std::thread ioc_thread_;
+
+        std::mutex map_mutex_;
+        get_cb_map get_cb_map_;
+        put_cb_map put_cb_map_;
     };
 
 }  // namespace lcxx
--- modules/server/src/server.cpp
+++ modules/server/src/server.cpp
@@ -12,14 +12,105 @@ using tcp       = boost::asio::ip::tcp;
 
 namespace lcxx {
 
-    server::server( std::string const & ip, uint16_t port ) {}
+    server::server( std::string const & ip, uint16_t port, std::optional< boost::asio::io_context > & ioc ) :
+        ioc_( ioc ? std::make_shared< net::io_context >( *ioc )
+                  : std::make_shared< net::io_context >( boost::asio::io_context{} ) ),
+        acceptor_( *ioc_, { net::ip::make_address( ip ), port } ),
+        socket_( *ioc_ ),
+        ioc_thread_(),
+        map_mutex_(),
+        get_cb_map_(),
+        put_cb_map_()
+    {
+    }
 
-    void server::on_get_endpoint( std::string const & endpoint, std::function< void() > callback ) {}
+    void server::on_get_endpoint( std::string const & endpoint, get_cb callback )
+    {
+        auto l = std::scoped_lock{ map_mutex_ };
+        get_cb_map_.insert_or_assign( endpoint, callback );
+    }
 
-    void server::on_put_endpoint( std::string const & endpoint, std::function< void() > callback ) {}
+    void server::on_put_endpoint( std::string const & endpoint, put_cb callback )
+    {
+        auto l = std::scoped_lock{ map_mutex_ };
+        put_cb_map_.insert_or_assign( endpoint, callback );
+    }
 
-    license server::read_license( request const & req, crypto::rsa_key_t private_key ) { return {}; }
+    auto server::read_license( request const & req, crypto::rsa_key_t private_key ) -> license { return {}; }
 
-    license server::write_license( request & req, crypto::rsa_key_t private_key ) { return {}; }
+    auto server::write_license( response & req, crypto::rsa_key_t private_key ) -> license { return {}; }
+
+    void server::run( run_option ro )
+    {
+        auto run_ioc = [this] {
+            auto work = boost::asio::make_work_guard( *ioc_ );
+            ioc_->run();
+        };
+
+        if ( ro == run_option::sync )
+            run_ioc();
+        else
+            ioc_thread_ = std::thread( run_ioc );
+    }
+
+    void server::stop() { ioc_->stop(); }
+
+    auto server::handle_loop() -> void
+    {
+        net::co_spawn( *ioc_, handle_loop_coro(), [this]( std::exception_ptr e ) {
+            if ( e )
+                std::rethrow_exception( e );
+        } );
+    }
+
+    net::awaitable< void > server::handle_loop_coro()
+    {
+        boost::system::error_code ec;
+        auto                      error = net::redirect_error( net::use_awaitable, ec );
+
+        while ( true ) {
+            ec = {};
+
+            co_await acceptor_.async_accept( socket_, error );
+            if ( ec )
+                continue;
+
+            beast::flat_buffer buffer{ 1024 * 1024 };
+            request            req;
+
+            auto bytes_transferred = co_await http::async_read( socket_, buffer, req, error );
+            if ( ec || bytes_transferred == 0 )
+                continue;
+
+            response resp;
+            resp.result( http::status::ok );
+
+            auto exec_cb = [&]( auto map ) {
+                if ( auto target = req.target().to_string(); map.contains( target ) ) {
+                    map.at( target )( req );
+                }
+            };
+
+            switch ( req.method() ) {
+            case http::verb::get:
+                exec_cb( get_cb_map_ );
+                break;
+            case http::verb::put:
+                exec_cb( put_cb_map_ );
+                break;
+
+            default:
+                resp.result( http::status::bad_request );
+                resp.set( http::field::content_type, "text/plain" );
+                beast::ostream( resp.body() ) << "Invalid request method - expected 'get' or 'put', but got "
+                                              << std::string( req.method_string() );
+                break;
+            }
+
+            auto bytes_written = co_await http::async_write( socket_, resp, error );
+            if ( ec || bytes_written == 0 )
+                continue;
+        }
+    }
 
 }  // namespace lcxx
