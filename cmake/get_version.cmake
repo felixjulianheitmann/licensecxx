961c6a1a9cf87965e9beaa75663ca80ebaef11fb
Felix <felixjulianheitmann@gmail.com>
2023-04-06 18:15:11 +0200

    Included version pull script

--- cmake/get_version.cmake
+++ cmake/get_version.cmake
@@ -10,7 +10,6 @@ execute_process(
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-message("Git describe: ${GIT_TAG}")
 string(REGEX MATCH "v.*\..*\..*" SEMVER_FOUND ${GIT_TAG})
 
 if("${SEMVER_FOUND}" STREQUAL "")
@@ -22,20 +21,21 @@ if("${SEMVER_FOUND}" STREQUAL "")
     set(GIT_VER_TAIL "dev")
 endif()
 
-string(REGEX MATCHALL "[0-9+]" VER_NUMBERS ${GIT_TAG})
+string(REGEX MATCHALL "[0-9+]\\.|[0-9+]\\-" VER_NUMBERS ${GIT_TAG})
+
+list(GET VER_NUMBERS 0 GIT_VER_MAJOR)
+list(GET VER_NUMBERS 1 GIT_VER_MINOR)
+list(GET VER_NUMBERS 2 GIT_VER_BUILD)
+list(GET VER_NUMBERS 3 GIT_VER_TAIL)
+string(REGEX REPLACE "([0-9+])\\." "\\1" GIT_VER_MAJOR ${GIT_VER_MAJOR})
+string(REGEX REPLACE "([0-9+])\\." "\\1" GIT_VER_MINOR ${GIT_VER_MINOR})
+string(REGEX REPLACE "([0-9+])\\-" "\\1" GIT_VER_BUILD ${GIT_VER_BUILD})
+string(REGEX REPLACE "([0-9+])\\-" "\\1" GIT_VER_TAIL ${GIT_VER_TAIL})
+
+string(JOIN "." GIT_VER_BASE ${GIT_VER_MAJOR} ${GIT_VER_MINOR} ${GIT_VER_BUILD})
+string(JOIN "+" GIT_VER_STR ${GIT_VER_BASE} ${GIT_VER_TAIL})
+string(JOIN "" GIT_VER_SEM "v" ${GIT_VER_STR})
 message("Git describe: ${GIT_TAG}")
-foreach(N ${VER_NUMBERS})
-    message(${N})
-endforeach()
-
-
-string(FIND ${GIT_TAG} "." VER_MAJOR_END)
-math(EXPR VER_MAJOR_END ${VER_MAJOR_END}-1)
-string(SUBSTRING ${GIT_TAG} 1 ${VER_MAJOR_END} GIT_VER_MAJOR)
-string(FIND ${GIT_TAG} "." VER_MINOR_END)
-math(EXPR VER_MINOR_END ${VER_MINOR_END}-1)
-string(SUBSTRING ${GIT_TAG} 1 ${VER_MINOR_END} GIT_VER_MAJOR)
-
 message("GIT_VER_SEM: ${GIT_VER_SEM}")
 message("GIT_VER_STR: ${GIT_VER_STR}")
 message("GIT_VER_MAJOR: ${GIT_VER_MAJOR}")
